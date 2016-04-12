2c996f0b634ab6a603cd553ba015eb7a498768ce
eg <eringoncer@gmail.com>
2016-04-12 13:50:21 -0700

    current snapshot html/css

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+
+var stickyOffset = $('.sticky').offset().top;
+
+$(window).scroll(function(){
+  var sticky = $('.sticky'),
+      scroll = $(window).scrollTop();
+
+  if (scroll >= stickyOffset) {
+    sticky.addClass('fixed');
+  } else {
+    sticky.removeClass('fixed');
+  }
+});
+
+
+
+});
